02b32a380e9bf75d965b10670c09597f154be881
Shosuke, Momotani <Shosuke.Momotani@sony.com>
2020-04-06 12:41:10 +0900

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <button v-on:click="read()">スタート</button>
+    <input type="checkbox" v-model="autoplay" />
+    <input type="number" v-model.number="duration" />
+
+    <p>{{ selected }}</p>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="item in cards" :key="item">{{ item }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  props: {},
+  data: function() {
+    return {
+      cards: require("../data/cards.json"),
+      selected: "",
+      autoplay: false,
+      duration: 5
+    };
+  },
+  methods: {
+    read: function() {
+      if (this.cards.length === 0) return;
+
+      // shuffle
+      (array => {
+        for (var i = array.length - 1; i > 0; i--) {
+          var r = Math.floor(Math.random() * (i + 1));
+          var tmp = array[i];
+          array[i] = array[r];
+          array[r] = tmp;
+        }
+      })(this.cards);
+
+      // pickup
+      this.selected = this.cards.pop();
+
+      // speech
+      const utter = new SpeechSynthesisUtterance(this.selected);
+      utter.onend = () => {
+        console.log(this.selected);
+        // autoplay
+        if (this.autoplay && this.cards.length > 0)
+          setTimeout(() => {
+            if (this.autoplay) this.read();
+          }, (this.duration - 1) * 1000 + Math.random() * 2000);
+      };
+      speechSynthesis.speak(utter);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    publicPath: '/karuta-reader/',
+    outputDir: 'docs',
+    devServer: {
+        port: 8080
+    }
+};
